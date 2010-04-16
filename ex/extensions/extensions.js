6d750409a4459bb97932649b7ff7d2678a2e9ed0
Pinhook <contact@pinhooklabs.com>
2010-04-15 21:02:04 -0500

    extensions are done

--- ex/extensions/extensions.js
+++ ex/extensions/extensions.js
@@ -1,4 +1,4 @@
-steal.plugins('jquery').then(
+steal.plugins('jquery', 'phui/tabs').then('scrollTo',
 	"../../jmvcdoc/resources/highlight",'../../jmvcdoc/resources/languages/javascript','../../jmvcdoc/resources/languages/www',
 function(){
 	 hljs.initHighlighting();
@@ -6,7 +6,17 @@ function(){
 		$("code").each(function(){
 			hljs.highlightBlock(this)
 		})
+		$('.tabs').phui_ui_tabs().parent().hide();
+		$('h3').click(function(){
+			var h3 = $(this)
+			h3.next().show('fast', function(){
+				
+				//$(window).scrollTop(h3.offset().top )
+			});
+			$.scrollTo(h3,'fast')
+		})
 	})
+		
 	
 })
 
@@ -28,4 +38,50 @@ function(){
 		}
 	})
 	$("#history").history();
+})
+.plugins('jquery/event/default').then(function(){
+	$('.toggler').live('default.toggle',function(){
+		$(this).toggleClass('toggle')
+	})
+	$('.toggler').live('click',function(){
+		$(this).trigger('toggle')
+	})
+	var count = 0;
+	$(document.documentElement).bind('toggle', function(ev){
+		if(++count > 5){
+			ev.preventDefault();
+		}
+	})
+})
+//then
+.then(function(){
+	$("#destroyME").bind("destroyed",function(){
+		alert('destroyed!')
+	})
+	$("#destroy").bind("click", function(){
+		$("#destroyME").remove();
+	})
+}).plugins('jquery/event/hover').then(function(){
+	$('.hovers').live('hoverenter', function(){
+		$(this).addClass('hover')
+	}).live('hoverleave', function(){
+		$(this).removeClass('hover')
+	})
+}).plugins('jquery/event/select').then( function(){
+	var sout, sin;
+	$('#select').delegate('.select',"selectin",function(ev){});
+	$('#select').delegate('.select',"selectout",function(ev){
+		if(!ev.relatedTarget){
+			$(this).hide('slow');
+		}
+	})
+}).plugins('jquery/dom/dimensions').then(function(){
+	$("#animateOuter").click(function(){
+		$('.hasPadding').animate({outerHeight: 100})
+	})
+}).plugins('jquery/dom/form_params').then(function(){
+	$("#fp").submit(function(e){
+		e.preventDefault();
+		$("#fpOut").text($.toJSON($(this).formParams()))
+	})
 })
