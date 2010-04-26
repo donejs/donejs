8ec60836106d942e35de3eb698a84d8651daea64
Brian Moschel <brian.moschel@gmail.com>
2010-04-26 00:28:39 -0500

    remove comma

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ SeleniumDefaults = {
 	// the domain where selenium will run
     serverHost: "localhost",
 	// the port where selenium will run
-    serverPort: 4444,
+    serverPort: 4444//,
 	// the domain/url where your page will run from (change if not filesystem)
     //browserURL: "http://localhost/"
 }
