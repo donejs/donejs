14a5d7c3093c5f928981fbd9acecef9b094d18a0
Pinhook <contact@pinhooklabs.com>
2010-05-04 10:43:52 -0500

    jmvc to produce docs

--- jmvc/jmvc.js
+++ jmvc/jmvc.js
@@ -2,4 +2,14 @@ steal.plugins('jquery/controller',
 'jquery/view','jquery/view/ejs'
 ,'jquery/view/jaml'
 ,'jquery/view/micro',
-'jquery/model')
+'jquery/model',
+'jquery/event/default',
+'jquery/event/destroyed',
+'jquery/event/drag',
+'jquery/event/drop',
+'jquery/event/hover')
+/**
+ * @class jQuery.event.special
+ * @tag core
+ */
+// as fasf sa
