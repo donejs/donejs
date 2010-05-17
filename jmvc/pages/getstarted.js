532a6ccf95d695b098c408c8ddc7088767f07881
Justin Meyer <justinbmeyer@gmail.com>
2010-05-17 00:58:20 -0500

    improved get started

--- jmvc/pages/getstarted.js
+++ jmvc/pages/getstarted.js
@@ -274,7 +274,112 @@ This does 2 things:
 </ol>
 When <code>cookbook/cookbook.js</code> runs, it loads a bunch of plugins, then loads the generated 
 controller and model.
+<h3>Get and Show Recipes and Recipe Form.</h3>
+<p>When recipe_controller.js is loaded, it creates Cookbook.Controllers.RecipeController.  
+RecipeController extends [jQuery.Controller controller]
+and describes what events control recipe functionality.
+</p>
 
+<p>
+Because RecipeController is a "document" controller 
+(<code>onDocument: true</code>), 
+it automatically listens on the document
+element for events described by it's prototype methods.  
+The <code>load</code> method listens for the window onload event and calls
+RecipeController's <code>load</code> function.  
+</p>
+<p>
+	The load function looks for a '#recipe' element in the page
+	and creates one if not present.
+	Then uses the <code>Recipe</code> model to retrieve a list of 
+	recipes and callback the <code>list</code> function.
+</p>
+<b>In  Recipe.findAll ....</b>
+<p>
+	An Ajax request is made to <code>/recipe</code>, but because the [jQuery.fixtures fixtures] plugin
+	is included, the ajax request is directed to <code>//cookbook/fixtures/recipes.json.get</code>.
+	After the content is retrived from the fixture, new instances of Recipe are created with the
+	[jQuery.Model.static.wrapMany wrapMany] function and passed to the success callback.
+</p>
+<div class='whisper'>
+	P.S. [jQuery.fixtures Fixtures] are awesome and help 
+	you develop while the slow-polk backend teams catch up.
+	Once the service is ready you simply have to remove 
+	the fixtures plugin from your application file.
+</div>
+<p>
+	The success function is RecipeController's <code>list</code> method.  
+	<code>List</code> replaces the "#recipe" element's 
+	html with the content rendered by the template in
+	<code>cookbook/views/recipe/init.ejs</code> with the 
+	recipe's data.
+</p>
+<p>
+	<code>cookbook/views/recipe/init.ejs</code> draws out the outline of the
+	recipe table and the recipe form.  It uses the partial template 
+	<code>'views/recipe/list'</code> to draw out the individual recipes.
+	
+</p>
+<div class='whisper'>
+	Multiple partial templates are used because other functionality will resuse them.
+</div>
+<h3>Create a Recipe.</h3>
+<p>RecipeController listens for "form submit".  It's important to note that 
+document controllers only respond to events in an element that has an id that matches
+the name of the controller.  In this case, RecipeController only responds to "form submit"
+events in "#recipe" element.</p>
+<p>When the event happens, the formParams plugin is used to turn the name
+and description fields into an object like:</p>
+@codestart
+{
+  name: "The entered name",
+  description : "The entered description" 
+}
+@codeend
+These attributes are passed to create a new recipe.  When 
+[jQuery.Model.prototype.save save] is called, Recipe model's
+create function is called with the recipe's attributes.
+In <code>Recipe.create</code> a post request is sent to
+"/recipes", but intercepted by the fixtures plugin.  Instead, fixtures call
+back success with a JSON object that looks like:
+@codestart
+{
+  "id": <u><b>100</b></u>,
+  "name": "The entered name",
+  "description" : "The entered description" 
+}
+@codeend
+<p>
+Success is the new recipe instance's [jQuery.Model.prototype.created created] function
+which updates the attributes of the recipe and publishes an OpenAjax "recipe.created" message. 
+</p>
+<p>"recipe.created subscribe" messages are listened for in RecipeController.  Here,
+RecipeController uses the list template to insert the new recipe's html into the page.</p>
+<h3>Destroy a Recipe.</h3>
+When a recipe's html "tr" element created, it is labeled with the recipe instance like this:
+@codestart html
+&lt;tr &lt;%= recipes[i]%> >
+@codeend
+This code adds  the following to the recipe element:
+<ul>
+	<li>the "recipe" class name</li>
+	<li>a unique identifier to the class name: <code>cookbook_models_recipe_5</code></li>
+	<li>the recipe instance to jQuery.data</li>
+</ul>
+Inside the tr, the destroy link look like this:
+@codestart html
+&lt;a class="destroy">destroy&lt;/a>
+@codeend
+Recipe controller listens for clicks on destroy in the  
+<code>'.destroy click'</code> action.  if the person wants to destroy that
+recipe, it uses <code>closest</code> to find the first parent with className= 
+'recipe' and then gets back the model instance.  With that instance, it calls destroy.
+<p>
+  [jQuery.Model.prototype.destroy] calls Recipe.destroy with the id of the object to be 
+  destroyed.  If successful, [jQuery.Model.prototype.destroyed] publishes a 
+  <code>"recipe.destroyed"</code> OpenAjax event.  RecipeController
+  listens for this event, then removes the element from the page.
+</p>
 */
 //break ----------------------------------------------------------------------
 
