1483cf8f385e9b81660e3e87d90d1a0f61cfe670
Brian Moschel <brian.moschel@gmail.com>
2010-10-18 00:33:55 -0400

    fixing tests

--- build.js
+++ build.js
@@ -1,24 +1,24 @@
 load('steal/rhino/steal.js')
 
-new steal.File("../jmvcdownload").removeDir()
-new steal.File("jmvcdownload.zip").remove()
-new steal.File("../jmvcdownload").mkdir()
+steal.File("../jmvcdownload").removeDir()
+steal.File("jmvcdownload.zip").remove()
+steal.File("../jmvcdownload").mkdir()
 
 var ignore = [".git", ".gitignore", "dist"]
 
-new steal.File("../jmvcdownload/documentjs").mkdir()
-new steal.File("documentjs").copyTo("../jmvcdownload/documentjs/", ignore)
+steal.File("../jmvcdownload/documentjs").mkdir()
+steal.File("documentjs").copyTo("../jmvcdownload/documentjs/", ignore)
 
-new steal.File("../jmvcdownload/funcunit").mkdir()
-new steal.File("funcunit").copyTo("../jmvcdownload/funcunit/", ignore)
+steal.File("../jmvcdownload/funcunit").mkdir()
+steal.File("funcunit").copyTo("../jmvcdownload/funcunit/", ignore)
 
-new steal.File("../jmvcdownload/jquery").mkdir()
-new steal.File("jquery").copyTo("../jmvcdownload/jquery/", ignore)
+steal.File("../jmvcdownload/jquery").mkdir()
+steal.File("jquery").copyTo("../jmvcdownload/jquery/", ignore)
 
-new steal.File("../jmvcdownload/steal").mkdir()
-new steal.File("steal").copyTo("../jmvcdownload/steal/", ignore)
+steal.File("../jmvcdownload/steal").mkdir()
+steal.File("steal").copyTo("../jmvcdownload/steal/", ignore)
 
-new steal.File("js").copyTo("../jmvcdownload/js", [])
-new steal.File("js.bat").copyTo("../jmvcdownload/js.bat", [])
+steal.File("js").copyTo("../jmvcdownload/js", [])
+steal.File("js.bat").copyTo("../jmvcdownload/js.bat", [])
 
-new steal.File("../jmvcdownload").zipDir("javascriptmvc-3.0.0.zip", "..\\jmvcdownload\\")
+steal.File("../jmvcdownload").zipDir("javascriptmvc-3.0.0.zip", "..\\jmvcdownload\\")
--- test/qunit/qunit.js
+++ test/qunit/qunit.js
@@ -1,7 +1,3 @@
 //we probably have to have this only describing where the tests are
 steal.plugins(	'funcunit/qunit', // load qunit
  				'jquery/test/qunit')
- 
-if(steal.browser.rhino){
-  steal.plugins('funcunit/qunit/env')
-}
