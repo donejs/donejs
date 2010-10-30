d08c20c4a7977e89c838962d27f39d0f1216d003
Brian Moschel <brian.moschel@gmail.com>
2010-10-30 18:35:24 -0500

    adding docs script

---
+++ scripts/docs.js
@@ -0,0 +1,15 @@
+_args = ['jmvc/jmvc.html']
+
+load('steal/rhino/steal.js');
+steal.overwrite = true;
+load('documentjs/documentjs.js');
+
+var file = _args.shift(),
+	options = steal.opts(_args || {}, {out: 1});
+
+
+DocumentJS(file,options);
+
+runCommand("cmd", "/C", "js jmvcdoc/toHTML/convert.js path=jmvc\\docs "+
+	"docsLoc=docs commentsLoc=http://jmvc.disqus.com/embed.js "+
+	"analyticsAct=UA-2302003-4 analyticsDomain=javascriptmvc.com")
