42dbdecefe48307fe6339fd8f2b27a981ed818c9
Brian Moschel <brian.moschel@gmail.com>
2010-11-01 02:43:41 -0500

    deploy script

--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -1,43 +1,18 @@
 require 'fileutils'
-require 'find'
-#require 'net/ftp'
 require 'rubygems'
-require 'aws/s3'
+require 'net/scp'
 
-AWS::S3::Base.establish_connection!(
-  :access_key_id     => '',
-  :secret_access_key => ''
-)
+# create the zip of everything
+system("js scripts/deploy.js")
 
-def send_files(ftp, path, omit_path)
-  Find.find(path) do |file_path|
-    if !file_path.include?(".git") && !file_path.include?(".jar") && !file_path.include?("deploy.rb")
-      remote_path = file_path
-      remote_path.slice!(0..1)
-      local_path = remote_path.gsub("/","\\")
-      begin
-      if FileTest.file?(file_path)
-        contents=nil
-        File.open(file_path, "rb") {|f| contents = f.read }
-        p "uploading "+local_path+" to "+remote_path
-        o = AWS::S3::S3Object.store(
-          remote_path,
-          contents,
-          "jmvcv3", 
-          :access => :public_read
-        )
-      end
-      rescue Exception => e
-          p e
-      end
-    end
-  end
+# ssh connect
+Net::SCP.start("javascriptmvc.com", "root", :keys => File.join("scripts", "key")) do |scp|
+    puts "uploading..."
+    puts scp.upload! "javascriptmvc.zip", "/opt"
+    puts "uploaded"
 end
 
-jmvc_path = File.join(File.dirname(__FILE__))
-#cd_jmvc_path = "cd "+jmvc_path+" && "
-#system("svn update")
-#system("js.bat apps/jmvc/compress.js")
-#system("js.bat apps/documentation/compress.js")
-#send_files(ftp, jmvc_path, "")
-send_files(nil, jmvc_path, "")
+Net::SSH.start("javascriptmvc.com", "root", :keys => File.join("scripts", "key")) do |ssh|
+	puts ssh.exec!("cp /opt/javascriptmvc.zip /u/apps/javascriptmvc/public/")
+	puts ssh.exec!("cd /u/apps/javascriptmvc/public/ && unzip -o javascriptmvc.zip")
+end
---
+++ scripts/pull_all.js
@@ -0,0 +1,8 @@
+
+runCommand("cmd", "/C", "git pull origin master")
+runCommand("cmd", "/C", "cd documentjs && git pull origin master")
+runCommand("cmd", "/C", "cd funcunit && git pull origin master")
+runCommand("cmd", "/C", "cd jmvcdoc && git pull origin master")
+runCommand("cmd", "/C", "cd jquery && git pull origin master")
+runCommand("cmd", "/C", "cd mxui && git pull origin master")
+runCommand("cmd", "/C", "cd steal && git pull origin master")
