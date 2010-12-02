c3187043354cec13382afea79f37abd9bc99007b
Brian Moschel <brian.moschel@gmail.com>
2010-12-02 04:09:12 -0600

    adding jquerymx stuff to deployment script

--- scripts/deployments/jmvc.js
+++ scripts/deployments/jmvc.js
@@ -4,4 +4,7 @@ runCommand("cmd", "/C", "js scripts/pull_all.js")
 
 runCommand("cmd", "/C", "js scripts/docs.js")
 
+runCommand("cmd", "/C", "js jquery/build.js")
+runCommand("cmd", "/C", "js jquery/buildAll.js")
+
 steal.File(".").zipDir("javascriptmvc.zip", ".\\")
