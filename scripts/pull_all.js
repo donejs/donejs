069a5d8e2f9f3f01d96202a1841d44c45dc87e6b
Brian Moschel <Brian.Moschel@contractor.cengage.com>
2011-10-13 12:21:08 -0400

    removing old scripts

--- scripts/pull_all.js
+++
@@ -1,9 +0,0 @@
-load('steal/rhino/utils.js')
-
-runCommand("cmd", "/C", "git pull origin master")
-runCommand("cmd", "/C", "cd documentjs && git pull origin master")
-runCommand("cmd", "/C", "cd funcunit && git pull origin master")
-runCommand("cmd", "/C", "cd jmvcdoc && git pull origin master")
-runCommand("cmd", "/C", "cd jquery && git pull origin master")
-runCommand("cmd", "/C", "cd mxui && git pull origin master")
-runCommand("cmd", "/C", "cd steal && git pull origin master")
