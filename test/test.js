bc01e5af92b2fe08f4fd3b92732a1c6d0d5da7d8
Brian Moschel <brian.moschel@gmail.com>
2012-01-21 15:23:46 -0500

    big jmvc test

--- test/test.js
+++ test/test.js
@@ -1,2 +1,12 @@
-steal.plugins(	'test/qunit','test/funcunit'
- 				)
+//we probably have to have this only describing where the tests are
+steal('funcunit')
+	.then('jquery/test/qunit')
+	.then('funcunit/test/funcunit')
+	.then('mxui/test.js')
+	
+	
+	// .then('steal/less/less_test.js')
+	// .then('funcunit/test/qunit')
+	.then(function(){
+		FuncUnit.jQuery("<iframe src='steal/test/qunit.html'></iframe>").appendTo(document.body)
+	})
