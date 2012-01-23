a0e5c428ab4b793c5e0d3b721465e3b00bb62f5d
Brian Moschel <brian.moschel@gmail.com>
2012-01-23 00:48:24 -0500

    big test passes consistently

--- test/test.js
+++ test/test.js
@@ -1,12 +1,9 @@
 //we probably have to have this only describing where the tests are
 steal('funcunit')
 	.then('jquery/test/qunit')
+	.then('funcunit/syn/test/qunit')
+	.then('funcunit/test/funcunit')
 	.then(function(){
 		FuncUnit.jQuery("<iframe src='steal/test/qunit.html'></iframe>").appendTo(document.body)
 	})
-	.then('funcunit/syn/test/qunit')
-	.then('funcunit/test/funcunit')
 	.then('mxui/test.js')
-	
-	
-	// .then('steal/less/less_test.js')
