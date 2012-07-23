9d4c802832fc1a1771a5bbba94e0edd83ebdaa11
Brian Moschel <brian.moschel@gmail.com>
2012-07-23 17:23:45 -0500

    latest steal

---
+++ stealconfig.js
@@ -0,0 +1,11 @@
+steal.config({
+	map: {
+		"*": {
+			'jquery/jquery.js' : "jquery",
+			"can/util/util.js": "can/util/jquery/jquery.js"
+		}
+	},
+	paths: {
+		"jquery": "can/util/jquery/jquery.1.7.1.js"
+	}
+})
