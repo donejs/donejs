be21fdfa673a8ce08ba33f95398955a9841efda1
David Luecke <daff@neyeon.com>
2012-07-24 15:29:26 -0600

    Updating references and docs

--- stealconfig.js
+++ stealconfig.js
@@ -1,11 +1,12 @@
 steal.config({
 	map: {
 		"*": {
-			'jquery/jquery.js' : "jquery",
+			'jquery/jquery.js' : "can/util/jquery/jquery.1.7.1.js",
 			"can/util/util.js": "can/util/jquery/jquery.js"
 		}
-	},
+	}
+	/* ,
 	paths: {
 		"jquery": "can/util/jquery/jquery.1.7.1.js"
-	}
+	} */
 })
