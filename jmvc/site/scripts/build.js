a426b3a0e5452d4873e117941f9cb44cf1f85d00
David Luecke <daff@neyeon.com>
2012-07-27 08:48:29 -0600

    Removing old JMVC folder, moving pages

--- jmvc/site/scripts/build.js
+++
@@ -1,6 +0,0 @@
-//steal/js jmvc/site/scripts/compress.js
-
-load("steal/rhino/rhino.js");
-steal('steal/build','steal/build/scripts','steal/build/styles',function(){
-	steal.build('jmvc/site/scripts/build.html',{to: 'jmvc/site'});
-});
