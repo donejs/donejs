02db9974da5c008c389a7172dca97a9930faf669
Justin Meyer <justinbmeyer@gmail.com>
2012-07-30 11:36:27 -0500

    updated to use arg docjs

--- site/scripts/doc.js
+++ site/scripts/doc.js
@@ -1,9 +1,11 @@
 load('steal/rhino/rhino.js');
-steal.overwrite = true;
-load('documentjs/documentjs.js');
 
-
-DocumentJS('site/scripts/doc.html',{
-	markdown : ['steal', 'can', 'site', 'jquery', 'funcunit', 'tutorials' ],
-	out : 'site/docs'
+steal("documentjs", function(DocumentJS){
+	DocumentJS('site/scripts/doc.html',{
+		markdown : ['steal', 'site'],
+		out : 'site/docs'
+	});
 });
+
+
+
