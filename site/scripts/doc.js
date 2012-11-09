834f0b3655652d9cd569aa5e4e764fc574c7444f
Nikica Jokic <neektza@gmail.com>
2012-11-08 18:57:08 -0600

    Docs update.

--- site/scripts/doc.js
+++ site/scripts/doc.js
@@ -2,7 +2,7 @@ load('steal/rhino/rhino.js');
 
 steal("documentjs", function(DocumentJS){
 	DocumentJS('site/scripts/doc.html',{
-		markdown : [ 'site', 'tutorials', 'steal', 'jquery', 'can', 'funcunit'],
+		markdown : [ 'site', 'tutorials', 'steal', 'jquery', 'can', 'funcunit' ],
 		out : 'site/docs'
 	});
 });
