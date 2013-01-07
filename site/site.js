de12f7e843d81ad21a7145cb76e2f360721c57a1
Brian Moschel <brian.moschel@gmail.com>
2013-01-06 21:06:07 -0600

    updating site app to 3.3

--- site/site.js
+++ site/site.js
@@ -3,7 +3,7 @@ steal.loadedProductionCSS = true;
 steal('steal/less')
 	.then('./stylesheets/site.less')
 	.then('jquery/controller', 'jquery/view/ejs')
-	.then('site/views/blog.ejs', function($){
+	.then('site/views/blog.ejs', function(){
 
 jQuery.Controller.extend('Feed',
 /* @Static */
@@ -30,7 +30,7 @@ jQuery.Controller.extend('Feed',
 	formatDate : function(date_string){
 		var date = new Date(date_string);
 		"Thu, 13 Jan 2011 11:59:47 -0800"
-		return $.String.sub(this.date_template, {
+		return can.sub(this.date_template, {
 			day: this.days[date.getDay()],
 			date: date.getDate(),
 			month: this.months[date.getMonth()],
@@ -51,7 +51,7 @@ Feed.extend('TwitterFeed',
 		$.ajax({
 			url: twitterUrl,
 			dataType: 'jsonp',
-			success: this.callback('insertTwitterFeed')
+			success: this.proxy('insertTwitterFeed')
 		});
 	},
 	insertTwitterFeed : function(data){
@@ -61,7 +61,7 @@ Feed.extend('TwitterFeed',
 			tweet.date = this.getDate(tweet.created_at)
 			if(tweet.text.charAt(0) != '@' && tweets.length < 6){
 				var formattedDate = this.formatDate(tweet.date);
-				tweets.push($.String.sub(this.template, {tweet: this.linkify(tweet.text), date: formattedDate}));
+				tweets.push(can.sub(this.template, {tweet: this.linkify(tweet.text), date: formattedDate}));
 			}
 		}
 		this.element.html('<ul>' + tweets.join('') + '</ul>');
@@ -86,7 +86,7 @@ Feed.extend('ForumFeed',
 /* @Static */
 {
 	loadData : function(data){
-		$('#forum-feed').data("controllers")['forum_feed'].insertForumFeed(data.value.items);
+		$('#forum-feed').data("controls")[0].insertForumFeed(data.value.items);
 	}
 },
 /* @Prototype */
@@ -111,7 +111,7 @@ Feed.extend('ForumFeed',
 					date   : this.formatDate(data[i].pubDate),
 					href   : data[i].link
 				}
-				html.push($.String.sub(this.template, d));
+				html.push(can.sub(this.template, d));
 			}
 		}
 		if(html.length > 0){
@@ -136,13 +136,13 @@ Feed.extend('BlogFeed',
 /* @Static */
 {
 	loadData : function(data){
-		$('#blog').data("controllers")['blog_feed'].insertBlogFeed(data.news);
+		$('#blog').data("controls")[0].insertBlogFeed(data.news);
 	}
 },
 /* @Prototype */
 {
 	init : function(){
-		var forumUrl = 'http://jupiterjs.com/news/feed/javascriptmvc.json?callback=BlogFeed.loadData';
+		var forumUrl = 'http://bitovi.com/news/feed/javascriptmvc.json?callback=BlogFeed.loadData';
 		$.ajax({
 			url: forumUrl,
 			dataType: 'script'
@@ -161,7 +161,7 @@ Feed.extend('BlogFeed',
 		var html = [], d, date, li, url;
 		for(var i = 0, ii = data.length; i < 6 && i < data.length; i++){
 			date = this.formatDate(data[i].publish_date);
-			url = "http://jupiterjs.com"
+			url = "http://bitovi.com"
 					+ data[i].url
 			d = {
 				title  : data[i].title,
