3cd7c283d180fe2e935918b4f4983f3f4cf2dd44
Justin Meyer <justinbmeyer@gmail.com>
2013-06-15 19:56:13 -0500

    updated to use latest docs

--- site/scripts/doc.js
+++ site/scripts/doc.js
@@ -4,6 +4,7 @@ steal("documentjs", function(DocumentJS){
 	DocumentJS('site/scripts/doc.html',{
 		markdown : [ 'readme.md', 'site', 'tutorials', 'steal', 'jquerypp', 'can', 'funcunit'],
 		out : 'docs',
-		parent: 'javascriptmvc'
+		parent: 'javascriptmvc',
+		"static": "site/static"
 	});
 });
