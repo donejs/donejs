aa602d69907b7ec2d19072fcaeaf545974339073
Chris Klanac <cklanac@gmail.com>
2015-09-15 11:55:39 -0400

    video animations

--- docs/theme/static/static.js
+++ docs/theme/static/static.js
@@ -2,19 +2,19 @@ steal("./content_list.js",
     "./frame_helper.js",
     "./versions.js",
     "./styles/styles.less!",
-    "./prettify",function(ContentList, FrameHelper, Versions){
+    "./prettify", function(ContentList, FrameHelper, Versions){
         var codes = document.getElementsByTagName("code");
-        for(var i = 0; i < codes.length; i ++){
+        for (var i = 0; i < codes.length; i++) {
             var code = codes[i];
-            if(code.parentNode.nodeName.toUpperCase() === "PRE"){
-                code.className = code.className +" prettyprint";
+            if (code.parentNode.nodeName.toUpperCase() === "PRE") {
+                code.className = code.className + " prettyprint";
             }
         }
         prettyPrint();
 
         new ContentList(".contents");
         new FrameHelper(".docs");
-        new Versions( $("#versions, .sidebar-title:first") );
+        new Versions($("#versions, .sidebar-title:first"));
 
 
         $('textarea').click(function(){
@@ -22,10 +22,10 @@ steal("./content_list.js",
         });
 
 
-        if($('.twitter-follow-button').length) {
+        if ($('.twitter-follow-button').length) {
             // replace the "Follow @canjs!" link with a little wiget with follower count.
             $('#twitter-wjs').remove();
-            !function (d, s, id) {
+            !function(d, s, id){
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (!d.getElementById(id)) {
                     js = d.createElement(s);
@@ -36,10 +36,12 @@ steal("./content_list.js",
             }(document, "script", "twitter-wjs");
         }
 
-        $('.triptych .box').hover(function(){}, function(){}).click(function(event){
+        $('.triptych .box').hover(function(){
+        }, function(){
+        }).click(function(event){
             var target = $(this).data('target');
             $('html, body').animate({
-                scrollTop: $( '#' + target ).offset().top
+                scrollTop: $('#' + target).offset().top
             }, 500);
 
         });
@@ -47,43 +49,45 @@ steal("./content_list.js",
             event.stopPropagation();
         });
 
-        (function(){
 
+        $('.performance-row .donejs').hover(
+            function(){
+                var row = $('.performance-row');
+                row.stop().animate({left: row.width() / 4}, 500);
+                $('.donejs-text').stop().animate({opacity: 1}, 500);
+                $('.naive-thumbs').stop().animate({opacity: 0.5}, 500);
+            },
+            function(){
+                var row = $('.performance-row');
+                row.stop().animate({left: 0}, 500);
+                $('.donejs-text').stop().animate({opacity: 0}, 500);
+                $('.naive-thumbs').stop().animate({opacity: 1}, 500);
+            }
+        );
 
-            $('.performance-row .donejs').hover(
+        $('.performance-row .naive').hover(
+            function(){
+                var row = $('.performance-row');
+                row.stop().animate({left: -(row.width() / 4)}, 500);
+                $('.naive-text').stop().animate({opacity: 1}, 500);
+                $('.donejs-thumbs').stop().animate({opacity: 0.5}, 500);
 
-                function(){
-                    var row = $('.performance-row');
-                    row.stop().animate({left:row.width() / 4}, 500);
-                    $('.donejs-text').stop().animate({opacity:1}, 500);
-                    $('.naive-thumbs').stop().animate({opacity:0.5}, 500);
-                },
-                function(){
-                    var row = $('.performance-row');
-                    row.stop().animate({left:0}, 500);
-                    $('.donejs-text').stop().animate({opacity:0}, 500);
-                    $('.naive-thumbs').stop().animate({opacity:1}, 500);
-                }
-            );
+            },
+            function(){
+                var row = $('.performance-row');
+                row.stop().animate({left: 0}, 500);
+                $('.naive-text').stop().animate({opacity: 0}, 500);
+                $('.donejs-thumbs').stop().animate({opacity: 1}, 500);
+            }
+        );
 
-            $('.performance-row .naive').hover(
-                function(){
-                    var row = $('.performance-row');
-                    row.stop().animate({left:-(row.width() / 4) }, 500);
-                    $('.naive-text').stop().animate({opacity:1}, 500);
-                    $('.donejs-thumbs').stop().animate({opacity:0.5}, 500);
-
-                },
-                function(){
-                    var row = $('.performance-row');
-                    row.stop().animate({left:0}, 500);
-                    $('.naive-text').stop().animate({opacity:0}, 500);
-                    $('.donejs-thumbs').stop().animate({opacity:1}, 500);
-                }
-            )
-
-        }())
-
+        $('video').hover(
+            function(el){
+                $(this).get(0).play();
+            },
+            function(el){
+                $(this).get(0).pause();
+            });
     });
 
 
