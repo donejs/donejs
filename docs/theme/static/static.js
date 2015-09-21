89f6e56a3f73be5bb3d82ed921cdb87d73ea49af
James Atherton <james@bitovi.com>
2015-09-21 15:21:38 -0500

    Made menu items automatically become active when scrolling, + misc style fixes.

--- docs/theme/static/static.js
+++ docs/theme/static/static.js
@@ -95,7 +95,77 @@ steal("./content_list.js",
             $(this).find('img').stop().animate({'margin-left':'12px'}, 225);
         }, function(){
             $(this).find('img').stop().animate({'margin-left':'0'}, 225);
-        })
+        });
+
+
+
+
+        var getNavToHeaderEl = function ( hEl ) {
+            var id = "";
+            if ( hEl.length ) {
+                id = hEl[ 0 ].id;
+            } else if ( hEl.id ) {
+                id = hEl.id;
+            } else if ( typeof hEl === "string" ) {
+                id = hEl;
+            }
+            if ( id.length === 0 ) {
+                return $();
+            }
+            return $( "section.contents a[href*='" + id + "']" );
+        };
+
+        $( "section.comment h3" ).each(function(){
+            //fix duplicate id problem
+            var ids = $( '[id="' + this.id + '"]' );
+            if( ids.length > 1 && ids[ 0 ] === this ) {
+                var navTo = getNavToHeaderEl( ids );
+                ids.each( function ( x, el ) {
+                    var aEl = navTo.get( x );
+                    aEl.href = aEl.href.replace( el.id, el.id + x );
+
+                    el.id = el.id + x;
+                });
+            }
+        });
+
+        var activeH2Li = $();
+        var doJQCollapsing = $( "body.Guide" ).length ? true : false;
+
+        if ( doJQCollapsing ) {
+            $( "section.contents ol ol" ).hide();
+        }
+
+        $(window).scroll(function () {
+            var el = document.elementFromPoint( ~~( document.body.offsetWidth / 2 ), 250 );
+            el = $( el ).closest( "section.comment > *" );
+            if ( !el.length ) {
+                return;
+            }
+            var h2 = el[ 0 ].tagName.toLowerCase() === "h2" ? el : el.prevAll( "h2:first" );
+            var h3 = el[ 0 ].tagName.toLowerCase() === "h3" ? el : el.prevAll( "h3:first" );
+
+            if ( !h2.length ) {
+                return;
+            }
+
+            var navToH2 = getNavToHeaderEl( h2 ).closest( "li" );
+            var navToH3 = navToH2.next( "ol" ).find( "li" ).has( getNavToHeaderEl( h3 ) );
+
+            $( "section.contents ol ol li.active" ).not( navToH3 ).removeClass( "active" );
+            navToH3.addClass( "active" );
+
+            var curH2Li = navToH2.closest( "li" );
+            if ( activeH2Li[ 0 ] !== curH2Li[ 0 ] ) {
+                activeH2Li.removeClass( "active" );
+                if ( doJQCollapsing ) activeH2Li.next( "ol" ).hide( 250 );
+
+                activeH2Li = curH2Li;
+
+                activeH2Li.addClass( "active" );
+                if ( doJQCollapsing ) activeH2Li.next( "ol" ).show( 250 );
+            }
+        });
 
     });
 
