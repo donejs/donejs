f3a69e36a2f887f538a2e6eafaedb2cb91e092d5
Matthew Phillips <matthew@matthewphillips.info>
2017-03-23 12:14:04 -0400

    Update main test file for PMO
    This updates the main test file for PMO which reflects that the
    functional tests are no longer in a separate file.
    
    This change is a result of
    https://github.com/donejs/generator-donejs/commit/2e9a7e92559761b1fa8aaf11dd0c20ce629c5ac3

--- guides/place-my-order/steps/test-runner/test.js
+++ guides/place-my-order/steps/test-runner/test.js
@@ -1,3 +1,16 @@
-import 'place-my-order/test/functional';
+import F from 'funcunit';
+import QUnit from 'steal-qunit';
 
 import 'place-my-order/restaurant/list/list-test';
+
+F.attach(QUnit);
+
+QUnit.module('place-my-order functional smoke test', {
+  beforeEach() {
+    F.open('./development.html');
+  }
+});
+
+QUnit.test('place-my-order main page shows up', function() {
+  F('title').text('place-my-order', 'Title is set');
+});
